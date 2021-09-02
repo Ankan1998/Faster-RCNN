02655c7b152b727c12ba603a81d50dc41cfdc65e
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-02 21:15:09 +0530

    label to indices converter script added

---
+++ utils/label_to_indices.py
@@ -0,0 +1,15 @@
+'''
+0 --> label to indices
+1 --> indices to label
+'''
+import pandas as pd
+from torch_snippets import *
+
+def label_idx_converter(cls_lst, ctype=0):
+    df = pd.DataFrame(flatten(cls_lst), columns=['label'])
+    label2idx = {l: t for t, l in enumerate(df['label'].unique())}
+    if ctype == 0:
+        return label2idx
+    else:
+        idx2label = {t: l for l, t in label2idx.items()}
+        return idx2label
