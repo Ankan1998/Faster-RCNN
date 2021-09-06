eda82aea287e80df1531f024bb6d0ce42c1198e5
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-06 21:04:05 +0530

    label_to_indices testing

--- utils/label_to_indices.py
+++ utils/label_to_indices.py
@@ -13,3 +13,9 @@ def label_idx_converter(cls_lst, ctype=0):
     else:
         idx2label = {t: l for l, t in label2idx.items()}
         return idx2label
+
+if __name__=="__main__":
+    csv_file = r'C:\Users\Ankan\Downloads\Aerial Maritime.v14-black_pad_one_pixel.tensorflow\train\_annotations.csv'
+    df = pd.read_csv(csv_file)
+    l2i = label_idx_converter(df['class'].tolist(),0)
+    print(len(l2i))
