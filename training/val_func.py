059b8aa25577c26767d2add7ca92cafd42a2d055
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-06 13:18:16 +0530

    validation_function added

---
+++ training/val_func.py
@@ -0,0 +1,14 @@
+import torch
+
+@torch.no_grad()
+def validate_batch(inputs, model,optimizer):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model.train()
+    input, targets = inputs
+    input = list(image.to(device) for image in input)
+    targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+    optimizer.zero_grad()
+    losses = model(input, targets)
+    loss = sum(loss for loss in losses.values())
+    return loss, losses
