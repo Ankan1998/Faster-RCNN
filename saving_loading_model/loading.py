c3b6754653d1688de5d0eb93d485041361888288
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-14 21:05:34 +0530

    loading for inference function added

--- saving_loading_model/loading.py
+++ saving_loading_model/loading.py
@@ -6,3 +6,8 @@ def load_checkpoint(checkpoint_path, model, optimizer):
     optimizer.load_state_dict(checkpoint['optimizer'])
     valid_loss = checkpoint['valid_loss_min']
     return model, optimizer, checkpoint['epoch'], valid_loss.item()
+
+def load_for_inference(checkpoint_path, model):
+    checkpoint = torch.load(checkpoint_path)
+    model.load_state_dict(checkpoint['state_dict'])
+    return model
